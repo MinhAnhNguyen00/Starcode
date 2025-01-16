aff530be12869e58996c7fd2caf80ec88f3fd63c
Christoph Huy <christoph.huy@campus.tu-berlin.de>
2025-01-16 17:45:38 +0100

    updated setup; now fails if css was not loaded

--- lib/scripts/setup_env.py
+++ lib/scripts/setup_env.py
@@ -3,11 +3,15 @@ import os
 
 from traitlets.config import get_config
 
-# load Jupyter config file
+# load Jupyter config file and setup custom css
 c = get_config()
 
-# add static path for css
+css_path = os.path.join(os.getcwd(), '.binder', 'custom', 'custom.css')
+if not os.path.exists(css_path):
+    raise FileNotFoundError(f'Custom CSS file not found at {css_path}')
+
 c.NotebookApp.extra_static_paths = [os.path.join(os.getcwd(), '.binder', 'custom')]
+c.NotebookApp.custom_display_url = '/static/custom/custom.css'
 
 # Matplotlib backend setup
-matplotlib.use("agg")
+matplotlib.use('agg')
